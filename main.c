a9b5486cd33aecd85fbea666f104c7389f2d5cfb
123Stan-The-Man123 <Molsas2020@gmail.com>
2024-09-17 21:24:42 +0100

    Sorted the code into functions and added some comments. Background processes are broken, will fix at some point.

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,74 +7,109 @@
 
 #define MAXLINE 100
 
+void main_loop(void);
+int get_tokens(char *input, char *delim, char *args[]);
+void cd(char *path);
+void fork_child(char *args[]);
+
 int main(void) {
-    char input[MAXLINE];
-    char *token;
-    char *args[MAXLINE];
-    char delim[2] = " ";
-    int i, len, cd, status;
+
+    main_loop();    /* Enter the main event loop */
+
+    return 0;
+}
+
+void main_loop(void) {
+    char input[MAXLINE];            /* String to store input */
+    char *args[MAXLINE];            /* Array of strings to store all arguments */
+    int i, len;
+    int background_process = 0;     /* Flag to check if the process is to be run in the background */
+
+    system("clear");                /* Clear the screen on launch */
     
-    while (1) {
-        token = "";
-
-        for (i = 0; i < MAXLINE; i++)
-            args[i] = NULL;
-
-        printf("myshell > ");
-        fflush(stdout);
-        fgets(input, MAXLINE, stdin);
+    while (1) {                         /* Infinite loop */
+        printf("myshell > ");           /* Print the user prompt */
+        fflush(stdout);                 /* Immediately print the prompt to avoid any conflicts */
+        fgets(input, MAXLINE, stdin);   /* Get the input from stdin */
 
         len = strlen(input);
 
-        input[len - 1] = '\0';
+        if (input[len - 1] == '\n')
+            input[len - 1] = '\0';                  /* Replace the newline with a nul terminator */
 
-        token = strtok(input, delim);
+        i = get_tokens(input, " ", args);   /* Gets all tokens from input */
 
-        if (token == NULL)
-            continue;
+        if (!strcmp(args[0], ""))    /* Skips the rest if no tokens entered */
+            continue; 
 
-        args[0] = token;
+        if (!strcmp(args[i], "&")) {      /* Sets the background flag to true and removes the '&' token */
+            background_process = 1;
+            i--;
+        }
 
-        for (i = 1; (args[i] = strtok(NULL, delim)) != NULL; i++)
-            ;
+        args[i+1] = NULL;     /* Replaces whatever the failed strtok returned with NULL */
 
-        args[i] = NULL;
-
-        if (!strcmp(token, "cd")) {
-            if (args[1] == NULL) {
-                chdir("/home");
-                continue;
-            }
-            cd = chdir(args[1]);
-
-            if (cd < 0)
-                printf("Invalid directory\n");
+        if (!strcmp(args[0], "cd")) {   /* Checks if command is cd */
+            cd(args[1]);
             
             continue;
         }
 
-        if (!strcmp(token, "exit")) {
+        if (!strcmp(args[0], "exit")) {     /* Checks if command is exit */
             system("clear");
             exit(0);
         }
         
-        else {
-            pid_t p = fork();
+        else 
+            fork_child(args);               /* Forks the parent process to handle the command */
 
-            if (p < 0) {
-                perror("fork failed");
-                exit(1);
-            }
+        if (!background_process)            /* Waits for process to finish unless it is a background process */
+            wait(NULL);
+        
+        else
+            background_process = 0;         /* Resets the flag otherwise */
+    }
+}
 
-            else if (p == 0) 
-                 if (execvp(token, args) < 0) {
-                    printf("execution failed: invalid command\n");
-                    exit(1);
-                }
+int get_tokens(char *input, char *delim, char *args[]) {
+    int i;
+
+    for (i = 0; i < MAXLINE; i++)       /* Initialises the token array with NULL */
+            args[i] = NULL;
+
+    args[0] = strtok(input, delim);     /* Gets the first token */
+
+    for (i = 1; (args[i] = strtok(NULL, delim)) != NULL; i++)   /* Proceeds to grab all tokens from input */
+        ;
+    
+    return --i;   /* Returns the position of the last token */
+}   
+
+void cd(char *path) {
+    int cd;
+
+    if (path == NULL) {         /* Defaults path to /home if empty */
+            chdir("/home");
+            return ;            /* Leave function early */
         }
 
-        wait(&status);
+        cd = chdir(path);       /* Collects the status code and potentially changes directory */
+
+        if (cd < 0)             /* Returns error message if an invalid path was given */
+            printf("Invalid directory\n");
+}
+
+void fork_child(char *args[]) {
+    pid_t p = fork();               /* Creates a forked process */
+
+    if (p < 0) {                    /* Checks for a failed fork */
+        perror("fork failed");
+        exit(1);
     }
 
-    return 0;
+    else if (p == 0) 
+        if (execvp(args[0], args) < 0) {                        /* Executes the command with tokens and returns an error if the call failed */
+            printf("execution failed: invalid command\n");
+            exit(1);
+        }
 }
