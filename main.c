7eae288c948d35b31f557379a2517ef4f2dc2a11
Stanislaw Molsa <138809482+123Stan-The-Man123@users.noreply.github.com>
2024-09-17 15:35:04 +0000

    Improvements to the shell.

--- main.c
+++ main.c
@@ -13,59 +13,68 @@ int main(void) {
     char *token;
     char *args[MAXLINE];
     char delim[2] = " ";
-    int i, len, cd;
+    int i, len, cd, status;
     
     while (1) {
-        pid_t p = fork();
+        token = "";
 
-        if (p < 0) {
-            perror("fork failed");
-            exit(1);
-        }
-
-        else if (p == 0) {
-            token = "";
-
-            for (i = 0; i < MAXLINE; i++)
-                args[i] = NULL;
-
-            printf("myshell > ");
-            fgets(input, MAXLINE, stdin);
-
-            len = strlen(input);
-
-            input[len - 1] = '\0';
-
-            token = strtok(input, delim);
-
-            args[0] = token;
-
-            for (i = 1; (args[i] = strtok(NULL, delim)) != NULL; i++)
-                ;
-
+        for (i = 0; i < MAXLINE; i++)
             args[i] = NULL;
 
-            if (!strcmp(token, "cd")) {
-                cd = chdir(args[1]);
+        printf("myshell > ");
+        fflush(stdout);
+        fgets(input, MAXLINE, stdin);
 
-                if (cd < 0)
-                   printf("Invalid directory\n");
+        len = strlen(input);
+
+        input[len - 1] = '\0';
+
+        token = strtok(input, delim);
+
+        if (token == NULL)
+            continue;
+
+        args[0] = token;
+
+        for (i = 1; (args[i] = strtok(NULL, delim)) != NULL; i++)
+            ;
+
+        args[i] = NULL;
+
+        if (!strcmp(token, "cd")) {
+            if (args[1] == NULL) {
+                chdir("/home");
+                continue;
+            }
+            cd = chdir(args[1]);
+
+            if (cd < 0)
+                printf("Invalid directory\n");
+            
+            continue;
+        }
+
+        if (!strcmp(token, "exit")) {
+            system("clear");
+            exit(0);
+        }
+        
+        else {
+            pid_t p = fork();
+
+            if (p < 0) {
+                perror("fork failed");
+                exit(1);
             }
 
-            if (!strcmp(input, "exit"))
-                exit(1);
-            
-            else
-                execvp(token, args);
+            else if (p == 0) 
+                 if (execvp(token, args) < 0) {
+                    printf("execution failed: invalid command\n");
+                    exit(1);
+                }
         }
 
-        else {
-            int status;
-            wait(&status);
-
-            if (status)
-                return 0;
-        }
+        wait(&status);
     }
 
     return 0;
