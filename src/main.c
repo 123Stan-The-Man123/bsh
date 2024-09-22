eb363575aea6323bd68ea61d94110a31b305dd9d
Gereon V <72784429+GereonV@users.noreply.github.com>
2024-09-22 20:19:51 +0200

    Refactor as suggestion

--- src/main.c
+++ src/main.c
@@ -8,174 +8,184 @@
 #include <unistd.h>
 #include "built-in.h"
 
-#define MAXLINE 1000
+#define MAXLINE 1000 // buffer size
+#define MAXPROMPT 201 // 200 characters + nul
 
-void main_loop(void);
-int get_tokens(char *input, char *delim, char *args[], int *reset);
-void fork_child(char *args[]);
+static void main_loop(void);
+static int get_tokens(char *input, bool *reset);
+static void fork_child(char *args[], bool background);
+static char *args[MAXLINE];
 
 int main(void) {
-
     using_history();
-
-    main_loop();    /* Enter the main event loop */
-
-    return 0;
+    main_loop();
 }
 
 void main_loop(void) {
-    char *input;            /* String to store input */
-    char *args[MAXLINE];            /* Array of strings to store all arguments */
-    int i, len;
-    int background_process = 0;     /* Flag to check if the process is to be run in the background */
-    char credentials[MAXLINE] = "@";
-    int reset_val = 0;
-    int *reset = &reset_val;
-
-    system("clear");                /* Clear the screen on launch */
+    system("clear");
     
-    while (1) {                     /* Infinite loop */
+    while (1) {
+        // Create PS: "@{login}-->{cwd} > "
+        char const * login = getlogin();
+        char const * cwd = getcwd(NULL, 0);
+        char prompt_string[MAXLINE];
+        prompt_string[0] = '\0';
+        if (login && cwd) {
+            prompt_string[0] = '@';
+            prompt_string[1] = '\0';
+            strlcat(prompt_string, login, MAXPROMPT);
+            strlcat(prompt_string, "-->", MAXPROMPT);
+            strlcat(prompt_string, cwd, MAXPROMPT);
+        }
+        free((void *) cwd);
+        strlcat(prompt_string, " > ", MAXPROMPT);
 
-        /* Gets the username and the current working directory to display as the prompt */
-        credentials[1] = '\0';
-        strcat(credentials, getlogin());
-        strcat(credentials, "-->");
-        strcat(credentials, getcwd(NULL, 0));
-        strcat(credentials, " > ");
 
-        /* Display the prompt and add the user's input to history */
-        input = readline(credentials);
+        // Display the prompt and add the user's input to history
+        char *input = readline(prompt_string);
+        if (!input) // EOF
+            exit(0);
         add_history(input);
 
-        len = strlen(input);
+        // TODO
+        /* // strip newline from end */
+        /* int len = strlen(input); */
+        /* if (input[len - 1] == '\n') */
+        /*     input[len - 1] = '\0'; */
 
-        if (input[len - 1] == '\n')
-            input[len - 1] = '\0';                  /* Replace the newline with a nul terminator */
-
-        i = get_tokens(input, " ", args, reset);   /* Gets all tokens from input */
-
-        if (i == -1) {      /* Continue if get_tokens failed*/
-            continue;
-        }
-
-        if (args[0] == NULL)    /* Continue if no tokens entered */
+        bool reset_val = false;
+        int i = get_tokens(input, &reset_val);
+        if (i == -1)
             continue;
 
-        if (!strcmp(args[i-1], "&") && i > 1) {      /* Sets the background flag to true and removes the '&' token */
-            background_process = 1;
-            i--;
+        // remove '&' token for background processes
+        bool background_process = false;
+        if (!strcmp(args[i - 1], "&") && i > 1) {
+            background_process = true;
+            args[--i] = NULL;
         }
-
-        args[i] = NULL;     /* Replaces whatever the failed strtok returned with NULL */
         
-        if (detect_builtin(args))      /* Detects if the command is a built-in command */
-            continue;
-
-        else 
-            fork_child(args);               /* Forks the parent process to handle the command */
-
-        if (!background_process)            /* Waits for process to finish unless it is a background process */
-            wait(NULL);
-        
-        else {
-            background_process = 0;         /* Resets the flag otherwise */
+        if (detect_builtin((char const **) args)) {
+            if (background_process)
+                puts("ignoring '&' for built-ins");
         }
+        else
+            fork_child(args, background_process);
 
-        if (*reset) {      /* Resets stdin and stdout if the flag is set */
-            *reset = 0;
-            printf("\n");
+        // reset redirections
+        if (reset_val) {
+            puts("");
             freopen("/dev/tty", "r", stdin);
             freopen("/dev/tty", "w", stdout);
         }
+        free(input);
     }
-
-    free(input);    /* Free the input during program termination */
 }
 
-int get_tokens(char *input, char *delim, char *args[], int *reset) {
+int get_tokens(char *input, bool *reset) {
+    char const *delim = " ";
+    memset(args, 0, sizeof args);
+
+    // 0 - normal, 1 - char found, 2 - redirected
+    int redirect_output = 0;
+    int redirect_input = 0;
+
+    args[0] = strtok(input, delim);
+
     int i;
-    int redirect_output_flag = 0;
-    int redirect_input_flag = 0;
-
-    for (i = 0; i < MAXLINE; i++)       /* Initialises the token array with NULL */
-            args[i] = NULL;
-
-    args[0] = strtok(input, delim);     /* Gets the first token */
-
-    for (i = 1; (args[i] = strtok(NULL, delim)) != NULL; i++) {   /* Proceeds to grab all tokens from input */
-        if (args[i][0] == '$' && args[i][1] != '\0') {       /* Checks if the token is an environment variable */
-            args[i]++;
-            args[i] = getenv(args[i]);
-
-            if (args[i] == NULL) {                     /* Returns an error if the variable is not set */
-                printf("Variable not found\n");
-                return -1;
-            }
+    for (i = 1; (args[i] = strtok(NULL, delim)); ++i) {
+        if (args[i][0] == '$' && args[i][1] != '\0') {
+            args[i] = getenv(args[i] + 1);
+            if (args[i])
+                continue;
+            printf("Variable not found\n");
+            return -1;
         }
 
-        else if (!strcmp(args[i], "<")) {
-            redirect_input_flag = 1;
+        if (!strcmp(args[i], "<")) {
+            if (redirect_input)
+                perror("multiple input redirections");
+            else
+                redirect_input = 1;
             continue;
         }
 
-        else if (!strcmp(args[i], ">")) {              /* Checks if the token is a redirection operator */
-            redirect_output_flag = 1;
+        if (!strcmp(args[i], ">")) {
+            if (redirect_output)
+                perror("multiple output redirections");
+            else
+                redirect_output = 1;
             continue;
         }
 
-        if (redirect_input_flag) {
-            redirect_input_flag = 0;
-            *reset = 1;
+        if (redirect_input == 1) {
+            redirect_input = 2;
 
-            if (!fopen(args[i], "r")) {            /* Checks for file */
-                printf("File not found\n");
-                return -1;
-            }
-            
-            if (freopen(args[i], "r", stdin) == NULL) {    /* Error check for freopen */
+            if (!freopen(args[i], "r", stdin)) {
                 perror("freopen failed");
                 return -1;
             }
-
-            args[i-1] = args[i] = NULL;         /* Resets part of the token array */
-
-            i -= 2;
         }
+        else if (redirect_output == 1) {
+            redirect_output = 2;
 
-        else if (redirect_output_flag) {        /* Checks for redirection */
-            redirect_output_flag = 0;
-            *reset = 1;
-
-            if (freopen(args[i], "w", stdout) == NULL) {    /* Error check for freopen */
+            if (!freopen(args[i], "w", stdout)) {
                 perror("freopen failed");
                 return -1;
             }
-
-            args[i-1] = args[i] = NULL;         /* Resets part of the token array */
-
-            i -= 2;
         }
+        else continue;
+
+        // Remove redirection tokens from args
+        args[i - 1] = NULL;
+        i -= 2;
     }
 
-    if (redirect_input_flag || redirect_output_flag) {                        /* Error check for redirection */
-        printf("error: missing file name\n");
+    if (redirect_input == 1 || redirect_output == 1) {
+        puts("error: missing file name");
         return -1;
     }
+
+    if (redirect_input || redirect_output)
+        *reset = true;
     
-    return i;   /* Returns the position of the last token */
+    return i;
 }
 
-void fork_child(char *args[]) {
-    pid_t p = fork();               /* Creates a forked process */
+void fork_child(char *args[], bool background) {
+    pid_t p = fork();
+    if (p < 0)
+        goto forkerror;
 
-    if (p < 0) {                    /* Checks for a failed fork */
-        perror("fork failed");
-        exit(1);
+    if (!background) {
+        if (!p)
+            goto exec;
+        wait(NULL);
+        return;
     }
 
-    else if (p == 0) 
-        if (execvp(args[0], args) < 0) {                        /* Executes the command with tokens and returns an error if the call failed */
-            printf("execution failed: invalid command\n");
-            exit(1);
-        }
+    if (!p) {
+        p = fork();
+        if (p < 0)
+            goto forkerror;
+        if (!p)
+            goto exec;
+        printf("spawned process: %d\n", p);
+        exit(0);
+    }
+    int rc;
+    wait(&rc);
+    if (rc)
+        exit(1);
+    return;
+
+    exec:
+    // Executes the command with tokens and returns an error if the call failed
+    execvp(args[0], args);
+    puts("execution failed: invalid command");
+    exit(1);
+
+    forkerror:
+    perror("fork failed:");
+    exit(1);
 }
